fabba3530d4f414c388abc8eec3995d51f1baf24
macstreet <macstreet23@gmail.com>
2023-01-18 16:09:25 -0700

    Challenge HTML and JS update

--- static/js/app.js
+++ static/js/app.js
@@ -24,11 +24,7 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-var filters = {};
-
-// 2. Attach an event to listen for changes to each filter
-
-d3.selectAll("input").on("change", updateFilters);
+var filters = [];
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
@@ -41,6 +37,8 @@ function updateFilters() {
 
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
+
+    console.log(filterId); 
       
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
@@ -64,13 +62,16 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    for (var i = 0; i < filters.length; i++) {
-      console.log(filters[i]);
-    };
+    Object.entries(filters).forEach(([key,value])=> {
+      filteredData = filteredData.filter(row => row[key]=== value);
+    });
     
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   }
+
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
 
   // Build the table when the page loads
   buildTable(tableData);
